975b0c1aa2a08328cba41f5cae53338c70681c27
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-08-14 09:40:10 -0700

    ans

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	int counter = 0;
+
+	while (counter < 10)
+	{
+		printf("%d", counter);
+		counter++;
+	}
+	printf("\n");
+	return (0);
+}	
