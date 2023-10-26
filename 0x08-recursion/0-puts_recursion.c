eeb7648fe0d10ceda8cf55fa04ac2ed8094dc4ff
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-10-26 04:51:47 -0700

    first

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - use recursion
+ *@s : parameter
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar (*s);
+		_puts_recursion(++s);
+	}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,10 @@
+#ifndef _main_H_
+#define _main_H_
+
+void _puts_recursion(char *s);
+
+
+
+
+
+#endif
