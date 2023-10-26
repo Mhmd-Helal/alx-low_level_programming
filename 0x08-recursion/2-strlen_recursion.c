04e7add66db1a6a4037928582a3ff67ee57f3f81
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-10-26 06:39:56 -0700

    ans

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *_strlen_recursion - lenth of string
- *@s : parameter
- *return : lenth of string
+ *@s: parameter
+ *Return: lenth of string
  */
 int _strlen_recursion(char *s)
 {
-	if(*s != '\0')
-	{	
-		return (1 + _strlen_recursion(s+1));	
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));	
 	}
-	return 0;
+	return (0);
 }
