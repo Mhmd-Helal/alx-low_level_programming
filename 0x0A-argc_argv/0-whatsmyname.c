38e7e954154ea38834e1e046f9a3f895e82b0b2d
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-10-27 08:06:53 -0700

    ans

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *@argc: number of cl arguments
+ *@argv: array of string
+ *Return: return 0
+ */
+int main(int argc ,char* argv[])
+{
+	(void) argc;
+	printf("%s\n",argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+hello world from 0x0A-argc_argv
