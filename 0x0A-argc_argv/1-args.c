87ebacef908a96c62f583e6a204f47b2b9ac7488
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-10-27 08:26:05 -0700

    ans

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ *main - main function
+ *@argc: num of argu
+ *@argv: vector of argu
+ *Return: always return 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
