275a29df7f4c757cd197b1d6776ed244e33ebd98
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-10-31 14:42:50 -0700

    ans

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ *_strlen - gets num of char in string
+ *@s: char of string to get num
+ *Return: len of charachter
+ */
+int _strlen(char *s)
+{
+	int i;
+	int total = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		total += 1;
+	}
+	return (total);
+}
