388f8b4b0a8b676d08ed8f1ae69eec32633b43c3
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-11-01 06:54:10 -0700

    ans

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,14 @@
 #include "main.h"
 #include "2-strlen.c"
 /**
- *
- *
- *
- *
+ *puts_half - print half string
+ *@str: string that are printed half
  */
 void puts_half(char *str)
 {
 	int i;
 
-	for (i = _strlen(str) / 2; i < _strlen(str) ; i++)
+	for (i = _strlen(str) / 2; i < _strlen(str) - 1; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
 }
