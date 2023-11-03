aaca1a9d7a243ca08d700d80d0b202b30088b53b
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-11-02 18:11:59 -0700

    ans

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *string_nconcat - add two string
+ *@s1: first string
+ *@s2: second string
+ *@n: num of string2
+ *Return: pointer to char of new string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *t;
+	int size;
+	int len = 0;
+	unsigned int i;
+	unsigned int j;
+
+	while (s1[len] != '\0')
+		len++;
+	size = sizeof(char) * (len + 1 + n);
+	t = malloc(size);
+	if (!t)
+		return (NULL);
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		t[i] = s1[i];
+	for (j = 0; j < n; j++)
+		t[i + j] = s2[j];
+	t[i + j] = '\0';
+	return (t);
+}
