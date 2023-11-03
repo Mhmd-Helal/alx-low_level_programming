117f530eeb75304f5b68476452a93c359fee5907
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-11-02 18:26:24 -0700

    ans

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,9 +22,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	t = malloc(size);
 	if (!t)
 		return (NULL);
-	for (i = 0; *(s1 + i) != '\0'; i++)
+	for (i = 0; i < len1; i++)
 		t[i] = s1[i];
-	for (j = 0; j < n; j++)
+	for (j = 0; j < len2; j++)
 		t[i + j] = s2[j];
 	t[i + j] = '\0';
 	return (t);
