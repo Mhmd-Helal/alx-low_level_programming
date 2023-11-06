69f995285f54fbcc1c23a49cfbc2c84aeea6faee
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2023-11-05 16:44:48 -0800

    ans

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *_strncat - concate two string with n byte of second string
+ *@dest: first string
+ *@src: second string
+ *@n: num of byte from second string
+ *Return: pointer to char
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len1 = 0;
+
+	while (dest[len1] != '\0')
+		len1++;
+	for (i = 0; i < n; i++)
+		dest[len1 + i] = src[i];
+	dest[len1 + n] = '\0';
+	return (dest);
+}
+
