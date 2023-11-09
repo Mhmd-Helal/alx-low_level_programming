1959f21f7b100b9571872dc1d1c54b7993d3df2c
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-08 21:42:37 -0800

    ans

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,15 +3,16 @@
 
 
 /**
- *
- *
- *
- *
+ *arry_iterator - print elements of array
+ *@array: array that are printed
+ *@size: size of array
+ *action: pointer to function
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
-
+	
+	if (array = NULL || action = NULL)
 	for (i = 0; i < size; i++)
 		action(array[i]);
 }
