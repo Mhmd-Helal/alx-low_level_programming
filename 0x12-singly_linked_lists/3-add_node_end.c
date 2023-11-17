c9ed3a777eb688edd662b928613b4c378d1f9e23
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-17 10:32:15 -0800

    ans

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *add_node_end - add item to end of list
+ *@head: first item in list
+ *@str: item
+ *Return: pointer to new item
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp;
+
+	int i = 0;
+
+	while (str[i])
+		i++;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = i;
+	new->next = NULL;
+	if (strdup(str) == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+		return (new);
+	}
+}
