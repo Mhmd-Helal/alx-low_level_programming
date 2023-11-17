e57be34b9b61e77151adee802fe9335d0c223db5
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-17 07:56:20 -0800

    ans

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *list_len - print len of  list
+ *@h: first node of list
+ *Return: len of list
+ */
+size_t list_len(const list_t *h);
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,7 +6,8 @@ typedef struct list_s{
 	int len;
 	struct list_s *next;
 }list_t;
-size_t print_list(const list_t *h);
 
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif
 
