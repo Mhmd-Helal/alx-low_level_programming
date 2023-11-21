88d5f196fcc72339beb61480bf7a90df7351e7c0
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-21 07:03:21 -0800

    ans

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,17 +8,27 @@
  *@n: the data
  *Return: pointer to the new node
  */
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *tmp;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	while (*head != NULL)
-		*head = *head->next;
-	*head->next = new;
-	return (new);
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		tmp = *head;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+		return (new);
+	}
 }
