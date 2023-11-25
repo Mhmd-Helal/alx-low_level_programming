d35b9347de1de22a1eb9604121674e24e3573688
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-25 14:30:02 -0800

    ans

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,16 +10,14 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int i = 1;
 
 	i = i << index;
+	if (index > 32)
+		return (-1);
 	if (n & i)
 	{
 		return (1);
 	}
-	else if ((n & i) == 0)
-	{
-		return (0);
-	}
 	else
 	{
-		return (-1);
+		return (0);
 	}
 }
