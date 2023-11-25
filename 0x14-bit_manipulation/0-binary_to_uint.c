285400b4c1f38d38aa3594ec40d3bb609437a338
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-25 08:48:28 -0800

    ans

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stddef.h>
+#include <math.h>
+/**
+ *binary_to_uint - convert bin to unsignedint
+ *@b: char of bin is 1 or 0
+ *Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int len = 0;
+	int i;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+		return (0);
+	while (b[len] != '\0')
+		len++;
+	for (i = 0; b[i] != '\0'; i++)
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		else
+		{
+			sum += (b[len - 1] - '0') * pow(2, i);
+			len--;
+		}
+	return (sum);
+}
