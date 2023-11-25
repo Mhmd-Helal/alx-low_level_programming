2f18ed5d637702356be59ec81f50d34fe883b8d6
mhmd-helal <mhmd.mhmd.helal.com>
2023-11-25 09:44:34 -0800

    ans

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,23 +7,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int len = 0;
-	int i;
-	unsigned int sum = 0;
+	int bin = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[len] != '\0')
-		len++;
-	for (i = 0; b[i] != '\0'; i++)
-		if (b[i] != '0' && b[i] != '1')
+	while(b[i] != '\0')
+	{
+		if (b[i] == '1')
 		{
-			return (0);
+			bin = bin << 1 | 1;
+		}
+		else if (b[i] == '0')
+		{
+			bin = bin << 1;
 		}
 		else
-		{
-			sum += (b[len - 1] - '0') * _pow(2, i);
-			len--;
-		}
-	return (sum);
+			return (0);
+		i++;
+	}
+	return (bin);
 }
