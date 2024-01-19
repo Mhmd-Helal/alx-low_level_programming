7271d3d42b0fe1c7d2aa74b40ab5aea7bed2f27f
Mohamed Helal <137654688+Mhmd-Helal@users.noreply.github.com>
2024-01-19 08:35:21 +0200

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,17 +10,21 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 10; i <= 19; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = 10; j <= 19; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i < 8 || j < 9)
+			if ((j % 10) > (i % 10))
 			{
-				putchar(',');
-				putchar(' ');
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				if (i != 18 || j != 19)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+
 		}
 	}
 	putchar('\n');
