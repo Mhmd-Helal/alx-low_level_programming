829fdda260eb8058d78f1c01b953c476446b964f
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-18 23:00:31 -0800

    ans

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,21 +9,25 @@ int main(void)
 {
 	int i;
 	int j;
-
-	for (i = 0; i <= 99; i++)
+	int k;
+	int l;
+	for (k = 0; i < 9; k++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (l = 0; l < 9; l++)
 		{
-			putchar(i / 10 + '0');
-			putchar(i % 10 + '0');
-			putchar(' ');
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-
-			if (!(i == 98 && j == 99))
+			for (i = 0; i <= 9; i++)
 			{
-				putchar(',');
-				putchar(' ');
+				for ( j = 0; j <= 9; j++)
+				for (j = 0; j <= 9; j++)
+				{
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(' ');
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
