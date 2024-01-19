22ce0cd621ced4f6431a9bc17380856c561d1fb2
Mohamed Helal <137654688+Mhmd-Helal@users.noreply.github.com>
2024-01-19 08:42:18 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,25 +9,21 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (k = 0; i < 9; k++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (l = 0; l < 9; l++)
+		for (j = i + 1; j <= 99; j++)
 		{
-			for (i = 0; i <= 9; i++)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
 			{
-				for (j = 0; j <= 9; j++)
-				{
-					putchar(k + '0');
-					putchar(l + '0');
-					putchar(' ');
-					putchar(i + '0');
-					putchar(j + '0');
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
