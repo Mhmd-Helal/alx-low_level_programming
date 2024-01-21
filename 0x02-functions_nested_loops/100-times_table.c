37de5bcfb50f58e8ea6a15a3d2ca94a7e7e205e7
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-21 09:37:40 -0800

    ans

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,27 +5,39 @@
  */
 void print_times_table(int n)
 {
-        int i, j, mul;
+        int i, j, mul, pro;
 	if (n > 15 || n < 0)
 		return;
-        for (i = 0; i < n; i++)
+        for (i = 0; i < n + 1; i++)
         {
-                for (j = 0; j < n; j++)
+                for (j = 0; j < n + 1; j++)
                 {
                         mul = i * j;
                         if (j == 0)
                                 _putchar('0');
-                        else if (mul > 9)
+			else if (mul > 99)
+			{
+				pro = mul % 100;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(mul / 100 + '0');
+				_putchar(pro / 10 + '0');
+				_putchar(pro % 10 + '0');
+
+			}	
+			else if (mul > 9)
                         {
                                 _putchar(',');
                                 _putchar(' ');
+				_putchar(' ');
                                 _putchar(mul / 10 + '0');
                                 _putchar(mul % 10 + '0');
-                        }
+                        }	
                         else
                         {
                                 _putchar(',');
                                 _putchar(' ');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(mul + '0');
 			}
