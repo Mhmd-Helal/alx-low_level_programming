c4354ac20e366e66667bbd1ae230f284e3091e58
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-21 09:01:08 -0800

    ans

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * times_table -  prints the 9 times table, starting with 0.
+ *
+ */
+void times_table(void)
+{
+        int i, j, mul;
+
+        for (i = 0; i < 10; i++)
+        {
+                for (j = 0; j < 10; j++)
+                {
+                        mul = i * j;
+                        if (j == 0)
+                                _putchar('0');
+                        else if (mul > 9)
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                                _putchar(mul / 10 + '0');
+                                _putchar(mul % 10 + '0');
+                        }
+                        else
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
+
