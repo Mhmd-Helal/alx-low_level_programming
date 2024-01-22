33821a39e8784ccfa5c5e04d4b15ed77f06ee552
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-21 20:45:58 -0800

    ans

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * print_ times_table -  prints the 9 times table, starting with 0.
- *
+ * print_times_table -  prints the 9 times table, starting with 0.
+ * @n: n times table
  */
 void print_times_table(int n)
 {
-        int i, j, mul, pro;
+	int i, j, mul, pro;
+
 	if (n > 15 || n < 0)
 		return;
-        for (i = 0; i < n + 1; i++)
-        {
-                for (j = 0; j < n + 1; j++)
-                {
-                        mul = i * j;
-                        if (j == 0)
-                                _putchar('0');
+	for (i = 0; i < n + 1; i++)
+	{
+		for (j = 0; j < n + 1; j++)
+		{
+			mul = i * j;
+			if (j == 0)
+				_putchar('0');
 			else if (mul > 99)
 			{
 				pro = mul % 100;
@@ -23,20 +24,19 @@ void print_times_table(int n)
 				_putchar(mul / 100 + '0');
 				_putchar(pro / 10 + '0');
 				_putchar(pro % 10 + '0');
-
-			}	
+			}
 			else if (mul > 9)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
+			{
+				_putchar(',');
 				_putchar(' ');
-                                _putchar(mul / 10 + '0');
-                                _putchar(mul % 10 + '0');
-                        }	
-                        else
-                        {
-                                _putchar(',');
-                                _putchar(' ');
+				_putchar(' ');
+				_putchar(mul / 10 + '0');
+				_putchar(mul % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(mul + '0');
