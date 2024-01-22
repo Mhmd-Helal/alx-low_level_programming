1c4b254bfaa15f8410124dee2024a64b2656c7ef
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-22 06:13:09 -0800

    ans

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_triangle -  prints a triangle
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = size; j > i; j--)
+			_putchar(' ');
+		for (k = 0; k < i + 1; k++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
