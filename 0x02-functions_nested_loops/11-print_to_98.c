ae38f5efdb250858a086a605686562f71e2bfd9a
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-21 20:35:26 -0800

    ans

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,25 @@
  */
 void print_to_98(int n)
 {
-        int i;
+	int i;
 
-        if (n > 98)
-        {
-                for (i = n; i >= 98; i--)
-                {
-                        printf("%i",i);
-                        if ( i != 98)
-                                printf(", ");
-                }
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
+		}
 
-        }
-        else
-        {
-                for (i = n; i <= 98; i++)
-                {
-                        printf("%i",i);
-                        if ( i != 98)
-                                printf(", ");
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%i", i);
+			if (i != 98)
+				printf(", ");
 		}
 	}
 	putchar('\n');
