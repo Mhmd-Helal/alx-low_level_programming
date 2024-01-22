839d04007afc1837c3520b9bf79d48fa6bf91329
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-22 04:55:23 -0800

    ans

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_square -  prints a square
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
