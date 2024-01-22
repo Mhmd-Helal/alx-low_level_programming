52d5d3ae06add247762756b24d381a4857c9db08
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-22 05:16:25 -0800

    ans

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * fizz_buzz - print
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i < 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			iprintf("%i ", i);
+	}
+}
+
