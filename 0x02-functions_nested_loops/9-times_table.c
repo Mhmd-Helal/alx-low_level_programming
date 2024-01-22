6ad834521a5f02570454022e96a65d431f4cf98d
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-21 20:29:06 -0800

    ans

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,26 +5,26 @@
  */
 void times_table(void)
 {
-        int i, j, mul;
+	int i, j, mul;
 
-        for (i = 0; i < 10; i++)
-        {
-                for (j = 0; j < 10; j++)
-                {
-                        mul = i * j;
-                        if (j == 0)
-                                _putchar('0');
-                        else if (mul > 9)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                                _putchar(mul / 10 + '0');
-                                _putchar(mul % 10 + '0');
-                        }
-                        else
-                        {
-                                _putchar(',');
-                                _putchar(' ');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			mul = i * j;
+			if (j == 0)
+				_putchar('0');
+			else if (mul > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(mul / 10 + '0');
+				_putchar(mul % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(' ');
 				_putchar(mul + '0');
 			}
