0ebae54cecc606a75656e7eb505a0194302ee738
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-01-31 07:54:35 -0800

    ans

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,15 @@
 #include "main.h"
 /**
- *
- *
- *
- *
+ * _strcmp - compare two string
+ * @s1: first string
+ * @s2: second string
+ * Return: value of string
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int total;
-	int len1 = 0;
 
-	while (s1[len1] != '\0')
-		len1++;
-	for (i = 0; i < len1; i++)
-		if(s1[i] == s2[i])
-			total = 0;
-		else
-			total += 1;
-	return (total);
+	for (i = 0; s1[i] - s2[i] == 0 && s1[i] != '\0'; i++)
+	{}
+	return (s1[i] - s2[i]);
 }
