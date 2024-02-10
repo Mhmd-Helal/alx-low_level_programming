d124fea112bb85eb634e66ed8d275c0407a61f69
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-02-10 06:40:05 -0800

    ans

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,13 +10,27 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
+	char *a;
+	char *b;
+
+	a = _strdup(a, name);
+	if (a == NULL)
+	{
+		return ( NULL);
+	}
+	b = _strdup(b, owner);
+	if (b == NULL)
+	{
+		free(a);
+		return (NULL);
+	}
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog != NULL)
 	{
-		new_dog->name = name;
+		new_dog->name = a;
 		new_dog->age = age;
-		new_dog->owner = owner;
+		new_dog->owner = b;
 	}
 	return (new_dog);
 }
