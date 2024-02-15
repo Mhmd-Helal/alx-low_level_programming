aa0a5bdf84ec16d50ed3c08b4fb819d65d0fd1fc
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-02-15 07:21:08 -0800

    ans

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,20 +10,20 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int len = 0;
-	int i;
 
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		len++;
-	}
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	fd = open(filename, O_WRONLY | O_CREAT, 00400 | 00200);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00400 | 00200);
 	if (fd == -1)
 		return (-1);
-	if (write(fd, text_content, len) == -1)
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		if (write(fd, text_content, len) == -1)
+			return (-1);
+	}
+	if (close(fd))
 		return (-1);
 	return (1);
 }
