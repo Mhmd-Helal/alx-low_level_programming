dc1077a05c72c8f22b0ecd8ca08eafa7498c79c5
mhmd-helal <mhmd.mhmd.helal@gmail.com>
2024-02-15 17:59:45 -0800

    ans

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUF_SIZE 1024
+/**
+ * main - program to copy
+ * @ac: argument count
+ * @av: array of arguments
+ * Return: 0 on sucess
+ */
+int main(int ac, char **av)
+{
+	char buf[BUF_SIZE];
+	int fd_r, fd_w;
+	ssize_t read_byts;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_r = open(av[1], O_RDONLY);
+	read_byts = read(fd_r, buf, BUF_SIZE);
+	if (fd_r == -1 || read_byts == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd_w = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_w == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	if (write(fd_w, buf, read_byts) == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	if (close(fd_r) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", fd_r);
+		exit(100);
+	}
+	if (close(fd_w) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", fd_w);
+		exit(100);
+	}
+	return (0);
+}
